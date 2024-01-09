e1ffc30fe20ebc84ac16e038377c8c0becc1b685
ronibarylko <ronibarylko@hotmail.com>
2024-01-09 14:17:19 -0300

    Use training script instead of training job to run local. WIP

--- src/training_job.py
+++ src/training_job.py
@@ -1,11 +1,14 @@
-from google.cloud import aiplatform
+import os
+
+from remote_training.training_script import TrainingScript
 
 
 class TrainingJob:
     DEFAULT_JOB_NAME = "cv-vertex-ai-trainer"
     CONTAINER_URI = "us-docker.pkg.dev/vertex-ai/training/pytorch-gpu.1-13.py310:latest"
 
-    def __init__(self, gc_project, gc_bucket, machine_type, accelerator_type, accelerator_count, training_config) -> None:
+    def __init__(self, gc_project, gc_bucket, machine_type, accelerator_type, accelerator_count,
+                 training_config) -> None:
         self.gc_project = gc_project
         self.gc_bucket = gc_bucket
         self.machine_type = machine_type
@@ -13,23 +16,15 @@ class TrainingJob:
         self.accelerator_count = accelerator_count
         self.training_config = training_config
 
-        aiplatform.init(project=self.gc_project, staging_bucket=self.gc_bucket)
+        env_vars = self._load_environment_variables()
 
-        self.job = aiplatform.CustomTrainingJob(
-            display_name=self.DEFAULT_JOB_NAME,
-            container_uri=self.CONTAINER_URI,
-            requirements=self._load_requirements(),
-            script_path="remote_training/training_script.py",
-        )
+        for key, value in env_vars.items():
+            os.environ[key] = str(value)
+
+        self.job = TrainingScript()
 
     def run(self):
-        self.job.run(
-            replica_count=1,
-            machine_type=self.machine_type,
-            accelerator_type=self.accelerator_type,
-            accelerator_count=self.accelerator_count,
-            environment_variables=self._load_environment_variables()
-        )
+        self.job.run()
 
     def _load_environment_variables(self):
         training_config = self.training_config
